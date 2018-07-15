32d225c49bb4860709f0f7d76cdd57611b7e45c7
Emerentius <emerentius@arcor.de>
2018-07-15 19:36:20 +0200

    p023: updated

--- 023_Non-abundant_sums/src/main.rs
+++ 023_Non-abundant_sums/src/main.rs
@@ -1,7 +1,15 @@
+#![feature(test)]
+extern crate test;
+
 fn prop_sum (nr : i32) -> i32 {
-	let mut proper_sum = 0;
-	for i in 1..nr/2+1 {
-		if nr%i == 0 { proper_sum += i }
+	let mut proper_sum = 1;
+	for i in 2..(nr as f64).sqrt() as i32 +1 {
+		if nr%i == 0 {
+			proper_sum += i;
+			if i*i != nr {
+				proper_sum += nr / i;
+			}
+		}
 	};
 	proper_sum
 }
@@ -9,22 +17,27 @@ fn prop_sum (nr : i32) -> i32 {
 fn main() {
 	// find abundant numbers
 	let mut abundant_numbers = vec![];
-	for i in 1..28123+1 { if prop_sum(i) > i { abundant_numbers.push(i) } }
+	for i in (2..28123+1).filter(|&i| prop_sum(i) > i) {
+		abundant_numbers.push(i)
+	}
 
 	println!("{} abundant numbers found.", abundant_numbers.len());
 	let mut abundant_summable = [false; 28123+1];
-	for &ab_nr1 in abundant_numbers.iter() {
-		for &ab_nr2 in abundant_numbers.iter() {
+	for (i, &ab_nr1) in abundant_numbers.iter().enumerate() {
+		for &ab_nr2 in abundant_numbers[i..].iter() {
 			let ab_sum = (ab_nr1 + ab_nr2) as usize;
 			if ab_sum <= 28123+1 { abundant_summable[ab_sum - 1] = true }
-			// abundant_summable [ summable_number ] = true
 		}
 	}
 
-	let mut sum = 0;
-	for (num, &summable) in (1..28123+1).zip(abundant_summable.iter()) {
-		if !summable { sum += num }
-	}
+	let sum = (1..28123+1).zip(abundant_summable.iter())
+		.filter(|&(_, &is_abundant)| is_abundant)
+		.fold(0, |sum, (num, _)| sum + num);
 	println!("{}", sum);
 
+}
+
+#[bench]
+fn bench(b: &mut test::Bencher) {
+	b.iter(main)
 }
