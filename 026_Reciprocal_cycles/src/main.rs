24375bcbe3d1b27259d0b21707da39f2a9aea1d8
Emerentius <emerentius@arcor.de>
2018-07-16 19:59:38 +0200

    p026: simplified

--- 026_Reciprocal_cycles/src/main.rs
+++ 026_Reciprocal_cycles/src/main.rs
@@ -1,24 +1,23 @@
-fn main() {
-	let mut rests = vec![];
-	let mut max_cycle = 0;
-	let mut temp;
-	let base = 10;
-	for i in 1..1000 {
-		let mut cycles;
-		temp = 1;
-		rests.push( temp );
-		'outer: while temp % i != 0 {
-			rests.push( temp*base );
-			temp = (temp*base) % i;
-			for (j, &rest_tb) in rests.iter().enumerate() {
-				if temp*base == rest_tb {
-					cycles = rests.len() - j + 1;
-					if cycles > max_cycle { max_cycle = cycles };
-					break 'outer;
-				}
-			}
+// rests of 1/divisor
+fn cycle_len(divisor: u32, rests: &mut Vec<u32>) -> usize {
+	rests.clear();
+	let mut rest = 1;
+	while rest % divisor != 0 {
+		rests.push(rest);
+		rest = (rest*BASE) % divisor;
+		if let Some(pos) = rests.iter().position(|&prev_rest| prev_rest == rest) {
+			return rests.len() + 1 - pos;
 		}
-		rests.clear();
 	}
+	0
+}
+
+const BASE: u32 = 10;
+
+fn main() {
+	let mut cache = vec![];
+	let max_cycle = (1..1000)
+		.map(|div| cycle_len(div, &mut cache))
+		.max().unwrap();
 	println!("{}", max_cycle);
 }
