832193eb6c60b7228f81f712306eda6139ec0667
Emerentius <emerentius@arcor.de>
2018-07-16 05:16:29 +0200

    p076: switch to generating function

--- 076_Counting_summations/src/main.rs
+++ 076_Counting_summations/src/main.rs
@@ -1,24 +1,30 @@
 #![feature(test)]
 extern crate test;
 
-fn n_possible_summations(num:u64, primes:&[u64] ) -> u64 {
-    match num {
-        0 => 1,
-        _ => {
-            primes.iter()
-                .take_while(|p| **p <= num)
-                .enumerate()
-                .fold(0, |sum, (i, &prime)| {
-                    sum + n_possible_summations(num-prime, &primes[i..])
-                })
-        },
-    }
-}
+fn pent_nr(n: i64) -> i64 { n*(3*n-1)/2 }
 
 fn main() {
-    let nums : Vec<_> = (1..101).collect();
-    let n = n_possible_summations(100, &nums[..]) - 1;
-    println!("{}", n);
+    // 0 and 1, each 1 way to write them
+    let mut p_func = vec![1, 1];
+
+    for n in 2..100+1 {
+        // compute next partition function recursively
+        let mut next_p = 0;
+        'k: for k in 1.. {
+            for pent in [k, -k].iter().cloned().map(pent_nr) {
+                if pent > n { break 'k }
+                match k % 2 == 0 {
+                    true =>  next_p -= p_func[(n - pent) as usize],
+                    false => next_p += p_func[(n - pent) as usize],
+                };
+            }
+        }
+
+        p_func.push(next_p);
+    }
+    // the above also counts n = n + 0 as a valid way
+    // therefore subtract 1
+    print!("{}", p_func[100]-1);
 }
 
 #[bench]
