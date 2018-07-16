2b1111282b6641971cc1bc48022a59fecfd3c628
Emerentius <emerentius@arcor.de>
2018-07-16 20:14:18 +0200

    p026: separate iteration and cycle detection

--- 026_Reciprocal_cycles/src/main.rs
+++ 026_Reciprocal_cycles/src/main.rs
@@ -1,15 +1,23 @@
-// rests of 1/divisor
+// detect cycles in the sequence of rests
+// reuse the cache
 fn cycle_len(divisor: u32, rests: &mut Vec<u32>) -> usize {
 	rests.clear();
-	let mut rest = 1;
-	while rest % divisor != 0 {
-		rests.push(rest);
-		rest = (rest*BASE) % divisor;
+	for rest in rests_iter(divisor) {
 		if let Some(pos) = rests.iter().position(|&prev_rest| prev_rest == rest) {
 			return rests.len() + 1 - pos;
 		}
+		rests.push(rest);
 	}
 	0
+}
+
+// create iterator over the rests of the divisions for each digit
+fn rests_iter(divisor: u32) -> impl Iterator<Item=u32> {
+	std::iter::repeat(()).scan(1, move |rest, ()| {
+		let old_rest = *rest;
+		*rest = (*rest * BASE) % divisor;
+		Some(old_rest)
+	}).take_while(|&rest| rest != 0)
 }
 
 const BASE: u32 = 10;
