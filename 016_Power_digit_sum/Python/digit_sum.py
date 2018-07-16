32adb601ded66ecfd0299b3e8fc35e161062371c
Emerentius <emerentius@arcor.de>
2018-07-16 08:55:17 +0200

    p016: golf

--- 016_Power_digit_sum/Python/digit_sum.py
+++ 016_Power_digit_sum/Python/digit_sum.py
@@ -1,9 +1,2 @@
 #!/usr/bin/env python3
-number = 2**1000
-string = str(number)
-
-digit_sum = 0;
-for char in string:
-        digit_sum += int(char)
-
-print(digit_sum)
+print( sum(int(ch) for ch in str(2**1000) ) )
