f437341bd87a19bd6625175fb2347176ef381e84
Emerentius <emerentius@arcor.de>
2018-07-29 00:48:12 +0200

    p030: fixed

--- 030_Digit_fifth_powers/src/main.rs
+++ 030_Digit_fifth_powers/src/main.rs
@@ -1,18 +1,18 @@
+#![feature(test)]
 extern crate test;
-use std::num::Int;
 
 fn digit_power_sum ( number : u64 ) -> u64 {
 	let string = number.to_string();
 	let mut sum :u64 = 0;
-	for grapheme in string.graphemes(true) {
-		sum += grapheme.parse::<u64>().unwrap().pow(5);
+	for grapheme in string.chars() {
+		sum += grapheme.to_digit(10).unwrap().pow(5) as u64;
 	}
 	sum
 }
 
 fn main() {
 	let upper_bound = 354294; // 354294 = 6 * 9^5 < 999_999
-	
+
 	let mut sum = 0;
 	for i in 10..upper_bound {
 		if i == digit_power_sum(i) { sum += i }
@@ -21,6 +21,6 @@ fn main() {
 }
 
 #[bench]
-fn bench(b: &mut test::Bencher) { 
+fn bench(b: &mut test::Bencher) {
 	b.iter(|| main());
 }
