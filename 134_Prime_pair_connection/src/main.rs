7c84332047d0d67b6d0035aca424c8614de84c2e
Emerentius <emerentius@arcor.de>
2018-07-28 23:40:43 +0200

    p134: fixed

--- 134_Prime_pair_connection/src/main.rs
+++ 134_Prime_pair_connection/src/main.rs
@@ -1,5 +1,3 @@
-#![feature(slice_patterns)]
-#![feature(step_by)]
 extern crate prime;
 
 fn digit_length(mut num: u64) -> u32 {
@@ -17,10 +15,10 @@ fn main() {
     let mut sum = 0;
     for (i,two_primes) in primes.windows(2).skip(2).enumerate() {
         if i % 1000 == 0 { println!("{}", i); }
-        if let [prime1, prime2] = two_primes {
+        if let &[prime1, prime2] = two_primes {
             let len = digit_length(prime1);
             let modu = 10u64.pow(len);
-            sum += (2*prime2..).step_by(prime2).find(|num| num % modu == prime1).unwrap();
+            sum += (2*prime2..).step_by(prime2 as usize).find(|num| num % modu == prime1).unwrap();
         }
     }
     println!("{}", sum);
