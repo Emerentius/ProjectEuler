e156cf9bc8ecc7eaabd4249357cc4fa1898bcb4a
Emerentius <emerentius@arcor.de>
2018-07-29 01:19:12 +0200

    p041: fixed

--- 041_Pandigital_prime/src/main.rs
+++ 041_Pandigital_prime/src/main.rs
@@ -1,3 +1,4 @@
+#![feature(test)]
 extern crate test;
 extern crate prime;
 
@@ -5,15 +6,15 @@ fn find_biggest_pandigital_prime ( prime_numbers:&[u64] ) -> Option<u64> {
 	'primes : for &prime in prime_numbers.iter().rev() {
 		let mut digits_found = [false;9];
 		let prime_string : String = prime.to_string();
-		for grapheme in prime_string.graphemes(true) {
-			let digit:u32 = grapheme.parse().unwrap();
+		for ch in prime_string.chars() {
+			let digit:u32 = ch.to_digit(10).unwrap();
 			if digit == 0 { continue 'primes };
 			if !digits_found[(digit-1) as usize] {
-				digits_found[(digit-1) as usize] = true; 
+				digits_found[(digit-1) as usize] = true;
 			}
 			else { continue 'primes; }
 		}
-		
+
 		for (n,&digit_present) in digits_found.iter().enumerate() {
 			if !digit_present { continue 'primes }; // failed
 			if n+1 == prime_string.len() { break }; // passed
@@ -26,7 +27,7 @@ fn find_biggest_pandigital_prime ( prime_numbers:&[u64] ) -> Option<u64> {
 fn main() {
 	let max_prime = 7654321; // every pandigital above divisible by 3
 	let prime_numbers = prime::sieve(max_prime);
-	
+
 	if let Some(biggest_pandigital_prime) = find_biggest_pandigital_prime(&prime_numbers) {
 		println!("{}", biggest_pandigital_prime);
 	}
