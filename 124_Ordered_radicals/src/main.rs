4654d26346c6e29e9dbe52195a797d675fa42fdf
Emerentius <emerentius@arcor.de>
2018-07-28 23:41:44 +0200

    p124: old solution (now broken)
    renamed stuff in prime library

---
+++ 124_Ordered_radicals/src/main.rs
@@ -0,0 +1,22 @@
+#![feature(test)]
+#![feature(slice_patterns)]
+extern crate test;
+extern crate prime;
+
+fn main() {
+    let limit = 100_000;
+    let primes = prime::sieve( (limit as f64).sqrt() as usize + 1 );
+    let mut rads = vec![(1,1)];
+    for n in 2..limit+1 {
+        let factors = prime::prime_factors(n as u64, &primes);
+        let rad = factors.into_iter().fold(1, |prod, [pr, _]| prod*pr);
+        rads.push((rad, n));
+    }
+    rads.sort();
+    println!("{:?}", rads[10_000 - 1].1);
+}
+
+#[bench]
+fn bench(b: &mut test::Bencher) {
+    b.iter(|| main())
+}
