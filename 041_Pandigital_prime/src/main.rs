5bb9878618c958944dc54a5b85979168ddc9bee1
Emerentius <emerentius@arcor.de>
2018-07-29 01:19:08 +0200

    p041: old solution (now broken)

---
+++ 041_Pandigital_prime/src/main.rs
@@ -0,0 +1,44 @@
+extern crate test;
+extern crate prime;
+
+fn find_biggest_pandigital_prime ( prime_numbers:&[u64] ) -> Option<u64> {
+	'primes : for &prime in prime_numbers.iter().rev() {
+		let mut digits_found = [false;9];
+		let prime_string : String = prime.to_string();
+		for grapheme in prime_string.graphemes(true) {
+			let digit:u32 = grapheme.parse().unwrap();
+			if digit == 0 { continue 'primes };
+			if !digits_found[(digit-1) as usize] {
+				digits_found[(digit-1) as usize] = true; 
+			}
+			else { continue 'primes; }
+		}
+		
+		for (n,&digit_present) in digits_found.iter().enumerate() {
+			if !digit_present { continue 'primes }; // failed
+			if n+1 == prime_string.len() { break }; // passed
+		}
+		return Some(prime);
+	}
+	None
+}
+
+fn main() {
+	let max_prime = 7654321; // every pandigital above divisible by 3
+	let prime_numbers = prime::sieve(max_prime);
+	
+	if let Some(biggest_pandigital_prime) = find_biggest_pandigital_prime(&prime_numbers) {
+		println!("{}", biggest_pandigital_prime);
+	}
+}
+
+#[bench]
+fn find_biggest_pandigital_prime_from_list (b:&mut test::Bencher) {
+	let primes = prime::sieve(7654321);
+	b.iter(|| find_biggest_pandigital_prime(&primes));
+}
+
+#[bench]
+fn find_primes (b:&mut test::Bencher) {
+	b.iter(|| prime::sieve(7654321));
+}
