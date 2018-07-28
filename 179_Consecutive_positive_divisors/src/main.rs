6a40ed4ca18f0ab0fcab61df1a428e5ad8a54a20
Emerentius <emerentius@arcor.de>
2018-07-28 23:18:11 +0200

    p179: fixed and switched from prime to primal

--- 179_Consecutive_positive_divisors/src/main.rs
+++ 179_Consecutive_positive_divisors/src/main.rs
@@ -1,15 +1,14 @@
-#![feature(slice_patterns)]
-extern crate prime;
+extern crate primal;
 
 fn main() {
-    let primes = prime::sieve(10us.pow(7)/2);
+    let primes = primal::Sieve::new(10usize.pow(7)/2);
     let mut n_divisors_last = 0;
     let mut count = 0;
-    for n in 2..10u64.pow(7) {
+    for n in 2..10_000_000 {
         if n % 1_000_000 == 0 { println!("{}", n) }
-        let factors = prime::prime_factors(n, &primes);
+        let factors = primes.factor(n).unwrap();
         let n_divisors = factors.into_iter()
-            .fold(1, |prod, [_,occ]| prod * (occ+1));
+            .fold(1, |prod, (_,occ)| prod * (occ+1));
         if n_divisors == n_divisors_last { count += 1 }
         n_divisors_last = n_divisors;
     }
