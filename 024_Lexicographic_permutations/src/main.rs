daacc88b8b7d806429b755368b9b5ef42ad68ec6
Emerentius <emerentius@arcor.de>
2018-07-29 00:42:33 +0200

    p024: fixed by switching to permutohedron

--- 024_Lexicographic_permutations/src/main.rs
+++ 024_Lexicographic_permutations/src/main.rs
@@ -1,11 +1,12 @@
-#![feature(collections)]
 #![feature(test)]
 extern crate test;
+extern crate permutohedron;
+use permutohedron::LexicalPermutation;
 
 fn main() {
-	let mut digits : [u64; 10]= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+	let mut digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 	for _ in 0..1_000_000-1 { digits.next_permutation(); }
-	let num = digits.iter().fold(0, |num, &dig| num * 10 + dig);
+	let num = digits.iter().fold(0, |num, &dig| num * 10 + dig as u64);
 	println!("{}", num);
 }
 
