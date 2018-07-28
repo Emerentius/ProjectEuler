2bc9b77d1fb2b1766f0e9409119ab20adc63de89
Emerentius <emerentius@arcor.de>
2018-07-29 01:17:20 +0200

    p040: fixed

--- 040_Champernownes_constant/src/main.rs
+++ 040_Champernownes_constant/src/main.rs
@@ -1,13 +1,11 @@
-use std::num::Int;
-
 fn main() {
 	let mut string = "0.".to_string();
 	for int in 1..1000000+1 { string.push_str( &int.to_string() ) }
-	
+
 	let mut product = 1;
 	for i in 1..6+1 {
-		let position = 10.pow(i) + 1; 
-		let digit = string[position..position+1].parse().ok().expect("");
+		let position = 10usize.pow(i) + 1;
+		let digit: u64 = string[position..position+1].parse().ok().expect("");
 		product *= digit;
 	}
 	println!("{}", product);
