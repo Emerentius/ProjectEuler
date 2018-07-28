73470341a02084d914d8d614316a73bf63283938
Emerentius <emerentius@arcor.de>
2018-07-28 22:08:49 +0200

    p204: fixed compilation, switched to primal

--- 204_Generalised_Hamming_Numbers/src/main.rs
+++ 204_Generalised_Hamming_Numbers/src/main.rs
@@ -1,17 +1,18 @@
-#![feature(collections)]
-#![feature(step_by)]
-extern crate prime;
-use std::collections::BitVec;
+extern crate primal;
+extern crate bit_vec;
+use bit_vec::BitVec;
+
+const MAX : usize = 1_000_000_000;
 
 fn main() {
-    const max : usize = 1_000_000_000;
-    let primes = prime::sieve(max);
-    let mut hamming_numbers = BitVec::from_elem(max+1, true);
+    let primes = primal::Primes::all().take_while(|&pr| pr < MAX)
+        .collect::<Vec<_>>();
+    let mut hamming_numbers = BitVec::from_elem(MAX+1, true);
     //zero doesn't count
     hamming_numbers.set(0, false);
 
     for prime in primes.into_iter().skip_while(|&p| p<100) {
-        for n in (prime as usize..max).step_by(prime as usize) {
+        for n in (prime as usize..MAX).step_by(prime as usize) {
             hamming_numbers.set(n, false);
         }
     }
