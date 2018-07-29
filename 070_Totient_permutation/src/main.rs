b5815869017db48b8ba0225d3aeb194829918d3b
Emerentius <emerentius@arcor.de>
2018-07-29 11:58:07 +0200

    p070: fixed

--- 070_Totient_permutation/src/main.rs
+++ 070_Totient_permutation/src/main.rs
@@ -1,19 +1,9 @@
-#![feature(step_by)]
-#![feature(collections)]
 extern crate prime;
-use std::collections::BitVec;
+extern crate bit_vec;
 
-fn phi (mut n: u64, factors: Vec<[u64;2]>) -> u64 {
+fn phi (n: u64, factors: Vec<[u64;2]>) -> u64 {
     n * factors.iter().fold(1, |prod, &[p,_]| (p-1)*prod )
     / factors.iter().fold(1, |prod, &[p,_]| p*prod )
-
-    /*let mut is_coprime = BitVec::from_elem( (n+1) as usize, true);
-    for &[prime, _] in factors.iter() {
-        for multiple in (prime..n+1).step_by(prime) {
-            is_coprime.set(multiple as usize, false);
-        }
-    }
-    (is_coprime.iter().filter(|&is| is).count() - 1) as u64*/
 }
 
 fn are_permutations(mut n1: u64, mut n2: u64) -> bool {
@@ -36,8 +26,7 @@ fn main() {
     let mut min_ratio_pos = 0;
 
     for i in (2..max).rev() {
-        //if i % 10_000 == 0 { println!("{}", i) }
-        let factors = prime::prime_factors(i, &primes);
+        let factors = prime::factors(i, &primes);
         let phi_i = phi(i, factors);
         if !are_permutations(i, phi_i) { continue }
         let ratio = i as f64 / phi_i as f64;
@@ -45,10 +34,7 @@ fn main() {
             min_ratio = ratio;
             min_ratio_pos = i;
             println!("n = {}, ϕ(n) = {} => {}", i, phi_i, ratio);
-            //println!("{}", min_ratio_pos);
         }
     }
-    //println!("{}", phi(87109, prime::prime_factors(87109, &primes)));
-
     println!("{}", min_ratio_pos);
 }
