8cf7f7aa125a2633a3be754b3695d67edf58d498
Emerentius <emerentius@arcor.de>
2018-07-29 11:56:02 +0200

    p069: solution

---
+++ 069_Totient_maximum/src/main.rs
@@ -0,0 +1,21 @@
+#![feature(test)]
+extern crate test;
+extern crate prime; // own library
+
+fn main() {
+    let primes = prime::sieve(100); // will escalate fast, think factorial
+    let mut number = 1;
+    for &prime in &primes {
+        number *= prime;
+        if number > 1_000_000 {
+            number /= prime;
+            break;
+        }
+    }
+    println!("{}", number);
+}
+
+#[bench]
+fn bench (b:&mut test::Bencher) {
+    b.iter(|| main())
+}
