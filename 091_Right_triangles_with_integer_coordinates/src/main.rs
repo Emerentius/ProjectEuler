4d7d6f21822149fa8e441da07d82a73ec9ffe7d5
Emerentius <emerentius@arcor.de>
2018-07-29 12:05:14 +0200

    p091: fixed

--- 091_Right_triangles_with_integer_coordinates/src/main.rs
+++ 091_Right_triangles_with_integer_coordinates/src/main.rs
@@ -1,21 +1,26 @@
 #![feature(test)]
-#![feature(custom_derive)]
 extern crate test;
-use std::cmp::max;
 use std::ops::{Add, Sub, Mul};
 
-#[derive(Debug, Copy, Eq, PartialEq, Add)]
-struct vector(i32, i32);
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct Vector(i32, i32);
 
-impl Sub for vector {
-    type Output = vector;
-    fn sub(self, other: Self) -> vector {
-        vector(self.0 - other.0, self.1 - other.1)
+impl Add for Vector {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Vector(self.0 + other.0, self.1 + other.1)
+    }
+}
+
+impl Sub for Vector {
+    type Output = Vector;
+    fn sub(self, other: Self) -> Vector {
+        Vector(self.0 - other.0, self.1 - other.1)
     }
 }
 
 // scalar product
-impl Mul for vector {
+impl Mul for Vector {
     type Output = i32;
     fn mul(self, other: Self) -> i32 {
         self.0 * other.0 + self.1 * other.1
@@ -24,20 +29,20 @@ impl Mul for vector {
 
 fn main() {
     let max_size = 50 + 1;
-    let p0 = vector(0,0);
+    let p0 = Vector(0,0);
 
     let mut count = 0;
     for x1 in 0..max_size {
         for y1 in 0..max_size {
 
-            let p1 = vector(x1, y1);
+            let p1 = Vector(x1, y1);
             if p1 == p0 { continue }
 
             'x2: for x2 in 0..max_size {
                 for y2 in 0..max_size {
                     if x2 <= x1 && y2 >= y1 { continue 'x2 }
 
-                    let p2 = vector(x2, y2);
+                    let p2 = Vector(x2, y2);
                     if p2 == p0 || p2 == p1 { continue }
 
                     if p1*p2 == 0
