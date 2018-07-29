f8fce9ae1c343e63243ec35cbf5cce4779ed5cde
Emerentius <emerentius@arcor.de>
2018-07-29 11:27:50 +0200

    p043: fixed

--- 043_Sub_string_divisibility/src/main.rs
+++ 043_Sub_string_divisibility/src/main.rs
@@ -1,24 +1,43 @@
+#![feature(test)]
 extern crate test;
-use std::num::Int;
+extern crate permutohedron;
+use permutohedron::LexicalPermutation;
+
+fn permutations(mut digits: [u64; 10]) -> impl Iterator<Item = [u64; 10]> {
+	let mut finished = false;
+	std::iter::repeat(())
+		.scan((), move |(), ()| {
+			let next = digits;
+			match (digits.next_permutation(), finished) {
+				(true, _) => Some(next),
+				(false, false) => {
+					finished = true;
+					Some(next)
+				}
+				(false, true) => None,
+			}
+		})
+}
 
 fn main() {
 	let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 	let primes = [2,3,5,7,11,13,17];
-	
+
 	let mut sum = 0;
-	'perm: for permutation in digits.permutations() {
+	'perm: for permutation in permutations(digits) {
 		if permutation[0] == 0 { continue } // not a pandigital
 		for (subslice, &prime) in permutation.windows(3).skip(1)
-					  .zip( primes.iter() ) {
-			let mut num : u64 = 0;
+			.zip( primes.iter() )
+		{
+			let mut num = 0;
 			for (j,&digit) in subslice.iter().rev().enumerate() {
-				num += digit * 10.pow(j as u32);
+				num += digit * 10u64.pow(j as u32);
 			}
-			
+
 			if num % prime != 0 { continue 'perm }
 		}
 		let perm_number = permutation.iter().rev().enumerate()
-				  .fold(0, |sum, (i,char)| sum + *char * 10.pow(i as u32) );
+				  .fold(0, |sum, (i,ch)| sum + *ch * 10u64.pow(i as u32) );
 		sum += perm_number;
 	}
 	println!("{}", sum);
