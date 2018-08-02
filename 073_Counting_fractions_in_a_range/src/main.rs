30cfef9a4c48677febb88fa34fa3b007a2c21042
Emerentius <emerentius@arcor.de>
2018-08-02 09:28:41 +0200

    p073: solution

---
+++ 073_Counting_fractions_in_a_range/src/main.rs
@@ -0,0 +1,15 @@
+#![feature(test)]
+extern crate test;
+extern crate prime;
+
+fn main() {
+    let count = prime::FareySequence::new(12_000).skip_while(|&(n,d)| !(n == 1 && d == 3) )
+        .take_while(|&(n,d)| !(n == 1 && d == 2))
+        .count();
+    println!("{}", count - 1); // 1/3 is counted, this corrects it
+}
+
+#[bench]
+fn bench(b: &mut test::Bencher) {
+    b.iter(|| main())
+}
