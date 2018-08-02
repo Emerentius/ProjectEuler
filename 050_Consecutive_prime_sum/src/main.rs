b086ef8b139a9c5732b205e5b6c71b0e9ebc2524
Emerentius <emerentius@arcor.de>
2018-08-02 09:01:12 +0200

    p050: solution

---
+++ 050_Consecutive_prime_sum/src/main.rs
@@ -0,0 +1,31 @@
+extern crate prime;
+
+fn main() {
+	let primes = prime::sieve(1_000_000);
+	
+	let mut max_seq_len = 0;
+	let mut max_base_prime = 0;
+	
+	for (i,&prime) in primes.iter().enumerate() {
+		let mut sum = 0;
+		
+		let iter1 = (&primes[..i]).iter().enumerate();
+		let mut iter2 = (&primes[..i]).iter();
+		let mut low = 0;
+		for (upp,&prime_upp) in iter1 {
+			sum += prime_upp;
+			while sum > prime {
+				if let Some(&prime_low) = iter2.next() {
+					sum -= prime_low;
+					low += 1;
+				}
+			}
+			let seq_len = upp as u64 - low + 1;
+			if sum == prime && seq_len > max_seq_len {
+				max_seq_len = seq_len;
+				max_base_prime = prime;
+			}
+		}
+	}
+	println!("prime: {}, sequence length: {}", max_base_prime, max_seq_len);
+}
