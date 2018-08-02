2af385ba011662827e801ba5b6f3aa296d5095ea
Emerentius <emerentius@arcor.de>
2018-08-02 09:01:12 +0200

    p055: solution

---
+++ 055_Lychrel_numbers/src/main.rs
@@ -0,0 +1,48 @@
+#![feature(test)]
+extern crate test;
+
+extern crate num;
+extern crate core;
+//use num::{BigUint, One, Zero};
+//use num::bigint::ToBigUint;
+
+fn reverse( num : u128 ) -> u128 {
+	let mut num = num.clone();
+	let base = 10;
+	let mut new_num: u128 = 0;
+	for _ in 0.. {
+		new_num = new_num * &base + &num % &base;
+		num = &num / &base;
+		if num == 0 { break };
+	}
+	new_num
+}
+
+fn main() {
+	let mut is_lychrel = [true;10_000];
+
+	'n: for n in 0..10_000 {
+		//if n % 100 == 0 { println!("{}", n); }
+
+		let mut num = n as _;
+		num = &num + reverse(num);
+		let mut reversed = reverse(num);
+		let mut nr_reversals = 1;
+
+		while &num != &reversed {
+			num = num + reversed;
+			reversed = reverse(num);
+			nr_reversals += 1;
+			if nr_reversals > 50 { continue 'n };
+		}
+		is_lychrel[n] = false;
+	}
+
+	let count = is_lychrel.iter().filter(|&&is_lych| is_lych).count();
+	println!("{}", count);
+}
+
+#[bench]
+fn bench(b: &mut test::Bencher) {
+	b.iter(main)
+}
