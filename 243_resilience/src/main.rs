580c5fea70af019848ccf21091d9621326c80db8
Emerentius <emerentius@arcor.de>
2018-08-02 11:02:13 +0200

    p243: solution

---
+++ 243_resilience/src/main.rs
@@ -0,0 +1,12 @@
+extern crate prime;
+
+fn main() {
+    let totients = prime::Phi32::new(1_000_000_000);
+    println!("{}",
+        (1..).zip(
+                totients.iter().map(|&phi| phi as usize)
+            )
+            .find(|&(n, phi)| phi*94744 < 15499*(n-1))
+            .unwrap().0
+    );
+}
