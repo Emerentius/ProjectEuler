3bdea8218a9c5b88efd1c0f14a58255050dc4807
Emerentius <emerentius@arcor.de>
2018-08-02 13:19:25 +0200

    p111: solution

---
+++ 111_Primes_with_runs/src/main.rs
@@ -0,0 +1,41 @@
+#![feature(step_by)]
+#![feature(test)]
+#![feature(collections)]
+extern crate test;
+extern crate prime;
+
+fn digits (mut num:u64) -> [u8;10] {
+    let mut digits = [0;10];
+    while num != 0 {
+        digits[(num % 10) as usize] += 1;
+        num /= 10;
+    }
+    digits
+}
+
+fn main() {
+    let primes = prime::prime_iter(10_000_000_000 - 1);
+    let mut sums = [[0;10];10]; // [dig][occ]
+
+    const limit : u64 = 1_000_000_000;
+    for prime in primes {//.skip_while(|&p| p < limit) {
+        if prime < limit { continue }
+        let digs = digits(prime);
+        for (dig,&occ) in digs.iter().enumerate() {
+            if occ >= 2 {
+                sums[dig][occ as usize] += prime;
+            }
+        }
+    }
+    let mut sum = 0;
+    for &sums_dig in sums.iter() {
+        sum += *sums_dig.iter().rev().find(|&&s| s > 0).unwrap();
+    }
+
+    println!("{}", sum);
+}
+
+#[bench]
+fn bench(b: &mut test::Bencher) {
+    b.iter(|| main())
+}
