85eb1c5b3a473f17f2b14ad2459dd298e63256e6
Emerentius <emerentius@arcor.de>
2018-08-02 10:55:29 +0200

    p214: solution

---
+++ 214_Totient_chains/src/main.rs
@@ -0,0 +1,25 @@
+#![feature(step_by)] // unstable for the time being
+
+fn main() {
+    const MAX: usize = 40_000_000;
+    const CHAIN_LEN: usize = 25;
+
+    let mut sum = 0;
+    let mut totients: Vec<_> = (0..MAX).collect();
+    let mut chain_len = vec![0; MAX];
+    chain_len[1] = 1; // offset of 1, applies to all chain lengths thereafter
+
+    for i in 2..MAX {
+        if totients[i] == i { // => i prime
+            for n in (i..MAX).step_by(i) {
+                totients[n] -= totients[n] / i;
+            }
+            if chain_len[totients[i]] + 1 == CHAIN_LEN {
+                sum += i;
+            }
+        }
+
+        chain_len[i] = chain_len[ totients[i] ] + 1;
+    }
+    println!("{}", sum);
+}
