421c0bc4c985cf4c3da397ea0dbcd2a01ff1fbe1
Emerentius <emerentius@arcor.de>
2018-08-02 10:55:28 +0200

    p187: solution

---
+++ 187_Semiprimes/src/main.rs
@@ -0,0 +1,21 @@
+#![feature(test)]
+extern crate test;
+extern crate prime;
+
+fn main() {
+    let max : u64 = 100_000_000;
+    let primes = prime::sieve( (max/2) as usize);
+
+    let mut count = 0;
+    for (i,&pr1) in primes.iter().enumerate() {
+        count += (&primes[i..]).iter()
+            .take_while(|&pr2| pr1*pr2 < max)
+            .count();
+    }
+    println!("{}", count);
+}
+
+#[bench]
+fn bench(b:&mut test::Bencher) {
+    b.iter(|| main())
+}
