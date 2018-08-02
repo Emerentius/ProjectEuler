b6096da58be29a802420bc3e4b34db63a92bf911
Emerentius <emerentius@arcor.de>
2018-08-02 10:27:04 +0200

    p092: solution

---
+++ 092_Square_digit_chains/src/main.rs
@@ -0,0 +1,30 @@
+#![feature(test)]
+extern crate test;
+
+fn dig_sq_sum(mut num : usize ) -> usize {
+	let mut sum = 0;
+	while num != 0 {
+		sum += (num % 10).pow(2);
+		num /= 10;
+	}
+	sum
+}
+
+fn last_in_chain ( num : usize ) -> usize {
+	match num {
+		0 => 0,
+		1 | 89 => num,
+		_ => last_in_chain( dig_sq_sum(num) ),
+	}
+}
+
+fn main() {
+	let prec: Vec<_> = (0..649).map(|n| last_in_chain(n)).collect();
+	let count = (1..10_000_000).filter(|&n| prec[dig_sq_sum(n)] == 89).count();
+	println!("{}", count);
+}
+
+#[bench]
+fn bench ( b : &mut test::Bencher ) {
+	b.iter(|| main() );
+}
