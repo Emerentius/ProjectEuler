1e9952e1fe601ccc51ffdbbd9081a398824ca760
Emerentius <emerentius@arcor.de>
2020-09-10 11:41:40 +0200

    p315: simplify

--- 315_digital_root_clocks/src/main.rs
+++ 315_digital_root_clocks/src/main.rs
@@ -27,20 +27,25 @@ impl SegmentMask {
         })
     }
 
-    fn segments_lit(self) -> u32 { self.0.count_ones() }
     fn n_segments_overlap(self, other: SegmentMask) -> u32 {
         (self.0 & other.0).count_ones()
     }
 }
 
-// compute array of segment masks for a given number
-fn masks(mut num: u64) -> Masks {
-    let mut masks = [SegmentMask::unlit(); 8];
-    let mut position = 0;
-    while num != 0 {
-        masks[position] = SegmentMask::new((num % 10) as u8);
+fn digits(mut num: u64) -> impl Iterator<Item = u8> {
+    std::iter::from_fn(move || {
+        if num == 0 { return None }
+        let digit = (num % 10) as u8;
         num /= 10;
-        position += 1;
+        Some(digit)
+    })
+}
+
+// compute array of segment masks for a given number
+fn masks(num: u64) -> Masks {
+    let mut masks = [SegmentMask::unlit(); 8];
+    for (mask, digit) in masks.iter_mut().zip(digits(num)) {
+        *mask = SegmentMask::new(digit);
     }
     masks
 }
@@ -48,18 +53,8 @@ fn masks(mut num: u64) -> Masks {
 fn count_transition_differences(old_masks: Masks, new_masks: Masks) -> u64 {
     old_masks.iter()
         .zip(new_masks.iter())
-        .fold(0, |sum, (&old_mask, &new_mask)| sum + 2*old_mask.n_segments_overlap(new_mask) as u64)
-}
-
-// compute digit sum for nums > 10, None used as iteration stop flag
-fn digit_sum(mut num: u64) -> Option<u64> {
-    if num < 10 { return None }
-    let mut sum = 0;
-    while num != 0 {
-        sum += num % 10;
-        num /= 10;
-    }
-    Some(sum)
+        .map(|(&old_mask, &new_mask)| 2*old_mask.n_segments_overlap(new_mask) as u64)
+        .sum()
 }
 
 fn main() {
@@ -70,7 +65,8 @@ fn main() {
         .map(|pr| pr as u64)
     {
         let mut last_masks = masks(num);
-        while let Some(new_num) = digit_sum(num) {
+        while num >= 10 {
+            let new_num = digits(num).map(|digit| digit as u64).sum();
             let new_masks = masks(new_num);
             sum += count_transition_differences(last_masks, new_masks);
             last_masks = new_masks;
